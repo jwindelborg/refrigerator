31a870ced6bab59fbe2977b3c78ba5d3db58c11f
Jesper Nielsen <jesper@windelborg.dk>
2017-09-06 12:20:09 +0200

    Make a first draft of automatic mode

--- ControlSoftware/main/main.ino
+++ ControlSoftware/main/main.ino
@@ -1,4 +1,4 @@
-#define RELAY_STATE_LED 11
+//#define RELAY_STATE_LED 11
 #define RELAY 7
 
 #define BTN_UP 10
@@ -8,7 +8,7 @@
 #define TEMP_SENSOR A0
 
 #define TARGET_TEMP_RAW 979
-#define BTN_DELAY 60
+#define LCD_DELAY 60
 
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
@@ -20,8 +20,10 @@ int btnDwnState = 0;
 int relayState = 0;
 int tempSensor = 0;
 
+int automaticMode = 1;
+
 void setup() {
-  pinMode(RELAY_STATE_LED, OUTPUT);
+  //pinMode(RELAY_STATE_LED, OUTPUT);
   pinMode(RELAY, OUTPUT);
   pinMode(BTN_UP, INPUT);
   pinMode(BTN_DOWN, INPUT);
@@ -42,24 +44,43 @@ void loop() {
   btnUpState = digitalRead(BTN_UP);
   btnDwnState = digitalRead(BTN_DOWN);
   
-  if (btnUpState == LOW) {
+  if(btnUpState == LOW) {
+    automaticMode = 0;
     relayState = 0;
     lcd.setCursor(6,1);
     lcd.print("off");
     digitalWrite(RELAY, HIGH);
-    digitalWrite(RELAY_STATE_LED, LOW);
+    //digitalWrite(RELAY_STATE_LED, LOW);
   }
   
-  if (btnDwnState == LOW) {
+  if(btnDwnState == LOW) {
+    automaticMode = 0;
     relayState = 1;
     lcd.setCursor(6,1);
     lcd.print("on ");
     digitalWrite(RELAY, LOW);
-    digitalWrite(RELAY_STATE_LED, HIGH);
+    //digitalWrite(RELAY_STATE_LED, HIGH);
+  }
+
+  if(automaticMode) {
+    // pump on if pump is not on, and temperature is too low
+    if(!relayState && tempSensor < TARGET_TEMP_RAW + 5) {
+      relayState = 1;
+      digitalWrite(RELAY, LOW);
+      lcd.setCursor(6,1);
+      lcd.print("on ");
+    }
+    // pump off if pump is on, and temperature is too high / high enough
+    if(relayState && tempSensor + 5 > TARGET_TEMP_RAW) {
+      relayState = 0;
+      digitalWrite(RELAY, HIGH);
+      lcd.setCursor(6,1);
+      lcd.print("off");
+    }
   }
   
   tempSensor = analogRead(TEMP_SENSOR);
   lcd.setCursor(6,0);
   lcd.print(tempSensor);
-  delay(BTN_DELAY);
+  delay(LCD_DELAY);
 }
